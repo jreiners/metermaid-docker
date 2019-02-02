fe99842edb80fe763a97e1f7ccc25f6454e47401
Justin Reiners <justin@hotlinesinc.com>
2019-02-01 21:35:51 -0600

    added missing weather script to github, renamed metermaid.yml to docker-compose.yml to aid in launching.
    Also added create table scripts to weather script, for first run.

--- weather/code/weather.py
+++ weather/code/weather.py
@@ -2,6 +2,10 @@ import requests
 import simplejson as json
 import mysql.connector
 
+createdb="CREATE DATABASE IF NOT EXISTS metermaid;"
+createtbl1="CREATE TABLE IF NOT EXISTS `utilities` (`mPrimaryKey` int(6) unsigned NOT NULL AUTO_INCREMENT,`mId` varchar(50) DEFAULT NULL,`mType` int(2) DEFAULT NULL,`mTime` int(11) DEFAULT NULL, `mTotalConsumption` int(11) DEFAULT NULL,`mConsumed` float DEFAULT NULL,PRIMARY KEY (`mPrimaryKey`)) ENGINE=InnoDB AUTO_INCREMENT=1408484 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;"
+createtbl2="CREATE TABLE IF NOT EXISTS `weatherdb` (`id` int(11) NOT NULL AUTO_INCREMENT,`epoch` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(), `mintemp` float DEFAULT NULL, `maxtemp` float DEFAULT NULL, `currenttemp` float DEFAULT NULL, `windspeed` float DEFAULT NULL, `winddir` varchar(3) DEFAULT NULL, `text1` varchar(30) DEFAULT NULL, `text2` varchar(30) DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=57915 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;"
+
 
 url='http://api.openweathermap.org/data/2.5/weather?q=Omaha&appid=fd3a41ccb4f3d1242381c0d007883e4d&units=imperial'
 
@@ -23,12 +27,16 @@ main=j['weather'][0]['main']
 q = "INSERT INTO metermaid.weatherdb (epoch, mintemp, maxtemp,currenttemp,windspeed,winddir,text1,text2) VALUES(NOW(),'{}','{}','{}','{}','{}','{}','{}');".format(min,max,currenttemp,windspeed,winddir,describe,main)
 
 cnx = mysql.connector.connect(user='root', password='weatherdb',
-                              host='weatherdb',
+                              host='127.0.0.1',
                               database='metermaid')
 print('inserting data')
 #print(q)
 print('opening cursor.')
 cursor = cnx.cursor()
+cursor.execute(createdb)
+cursor.execute(createtbl1)
+cursor.execute(createtbl2)
+
 print('executing mysql insert')
 cursor.execute(q)
 print('committing')
