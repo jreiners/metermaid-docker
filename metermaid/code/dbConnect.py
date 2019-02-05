7fa6c7a4a77335078eec9010129635ee58fa8789
Justin Reiners <justin@reiners.io>
2019-02-05 15:51:04 -0600

    pushing changes, cleaned up git repo, removed unused/unneeded files...

--- metermaid/code/dbConnect.py
+++
@@ -1,2 +0,0 @@
-# ReName this file to dbConnect.py fill in hostname (eg. localhost or 127.0.0.1) and password
-dbConn  = pymysql.connect(user='root', password='weatherdb', host='127.0.0.1', database='metermaid', autocommit=True)
